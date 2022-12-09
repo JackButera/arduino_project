3d4dfaed24ecf9853e6581266d8b0c09d91f1ea8
Jack Butera <jackjrbutera@gmail.com>
2022-12-09 11:13:02 -0500

    webpage is working for one arduino rtu

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+from flask import Flask, jsonify, render_template, request
 import socket
-import threading
 import array
-import pickle
-from flask import Flask
-
 
 app = Flask(__name__)
 
@@ -13,14 +10,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = "192.168.1.180"
 port = 5000
 
-# Creating socket
-s.bind((ip, port))
-
 recvIP = "192.168.1.177"
 recvPort = 8888
 
 sendCommand = [0xAA, 0xFC, 177, 0x03]
 
+# BCH checksum
 def DCP_genCmndBCH(buff, count):
     nBCHpoly = 0xB8
     fBCHpoly = 0x7F
@@ -39,11 +34,13 @@ def DCP_genCmndBCH(buff, count):
     bch ^= fBCHpoly
     return bch
 
+# Adding checksum to command array
 commandBCH = DCP_genCmndBCH(sendCommand, 4)
 sendCommand.append(commandBCH)
 
 byte_array = bytearray(sendCommand)
 
+# Return string for temperature threshold
 def getTempState(thresh):
     if (thresh == 0b0101):
         return "Major Under"
@@ -56,6 +53,7 @@ def getTempState(thresh):
     elif (thresh == 0b1010):
         return "Major Over"
 
+# Validates response packet from Arduino
 def validateFAPacket(arr):
     recBCH = arr[4]
     checkBCH = DCP_genCmndBCH(arr, 4)
@@ -63,50 +61,48 @@ def validateFAPacket(arr):
         return True
     return False
 
+# Validates data packet from Arduino
 def validateDataPacket(arr):
     recBCH = arr[7]
     checkBCH = DCP_genCmndBCH(arr, 7)
     if (recBCH == checkBCH):
         return True
     return False
-
+    
 
 # Function for receiving messages
 def receiveMessages():
-    while(True):
-        data = s.recvfrom(1024)
-        data = data[0]
-        if (validateFAPacket(data[:5]) & validateDataPacket(data[5:])):
-            state = getTempState(data[8])
-            temp = data[9]
-            print(f'Temperature State: {state}')
-            print(f'Temperature: {temp}')
-        else:
-            print(f'Failed to validate BCH')
+    data = s.recvfrom(1024)
+    data = data[0]
+    if (validateFAPacket(data[:5]) & validateDataPacket(data[5:])):
+        state = getTempState(data[8])
+        temp = data[9]
+        return 'Temperature State: ' + str(state) + ' -> Temperature: '+ str(temp)
+    else:
+        return 'Failed to validate BCH'
         
 
 # Function for sending messages
 def sendMessages():
     s.sendto(byte_array, (recvIP, recvPort))
 
+## Sets ip address, only runs once when program is first run
+@app.before_first_request
+def do_something_only_once():
+    s.bind((ip, port))
 
+# Gets new values for temperature state and temperature
+@app.route('/_stuff', methods = ['GET'])
+def stuff():
+    sendMessages()
+    ret = receiveMessages()
+    return jsonify(result=ret)
 
+# Displays temperature state and temperature
+@app.route('/')
+def Home():
+    return render_template('dy1.html')
 
-def printit():
-    threading.Timer(5.0, printit).start()
-    receive = threading.Thread(target=receiveMessages)
-    send = threading.Thread(target=sendMessages)
-    receive.start()
-    send.start()
-
-printit()
-    
-
-
-# @app.route("/")
-# def home():
-    
-#     return "Hello, World!"
-    
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# Runs flask
+if __name__ == "__main__":
+    app.run(debug=True)
